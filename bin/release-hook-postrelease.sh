68050eef5e80d97b5838816f02ac3f8fa91de790
Patrick Kollitsch <patrick@davids-neighbour.com>
2020-10-21 21:51:55 +0700

    chore: cleanup and update setup, removing lots of cruft

--- bin/release-hook-postrelease.sh
+++ bin/release-hook-postrelease.sh
@@ -2,11 +2,3 @@
 
 # push to repo
 git push --follow-tags origin main
-
-# retrieve github token
-GREN_GITHUB_TOKEN=$(grep GREN_GITHUB_TOKEN .env | xargs)
-IFS='=' read -ra GREN_GITHUB_TOKEN <<<"$GREN_GITHUB_TOKEN"
-GREN_GITHUB_TOKEN=${GREN_GITHUB_TOKEN[1]}
-
-# create release
-gren release --token="$GREN_GITHUB_TOKEN" --config .grenrc.js
