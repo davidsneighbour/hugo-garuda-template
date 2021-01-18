fe38fbc960000ff024fd1a6db7dcdc0e57a7f36f
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-01-18 19:13:26 +0700

    chore: update release hook script system to work with replace config

--- bin/netlify.sh
+++ bin/netlify.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-git config --global --add url."git@github.com:".insteadOf "https://github.com/"
+################################################################################
+# A bash script to publish your Hugo website to Netlify.
+# Opiniated like any other DNB product :)
+################################################################################
+
+# updating submodules
+git submodule update --init --recursive --depth=1
+
+# create webpack files
+npm run build:webpack
+
+# updating modules
+hugo mod get -u ./...
+
+# compiling the site
 hugo --gc
+
+# commit algolia index
+#atomic-algolia
+
+# removing files that are not required to exist in the global scope
 rm -rf public/posts
