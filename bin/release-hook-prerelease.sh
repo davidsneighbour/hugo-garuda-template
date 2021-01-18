04912a065573cbbefff7e08962fd283365c5b410
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-01-18 22:40:08 +0700

    fix: replacement generation for release scripts is broken

--- bin/release-hook-prerelease.sh
+++ bin/release-hook-prerelease.sh
@@ -2,7 +2,7 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-if test -f "$SCRIPTPATH"; then
+if test -f "$SCRIPTPATH"/replacements; then
   while read -ra __; do
     go mod edit -dropreplace ${__[0]}
   done < "$SCRIPTPATH"/replacements
@@ -18,7 +18,7 @@ git add go.sum
 #hugo
 #git add public/ -f
 
-if test -f "$SCRIPTPATH"; then
+if test -f "$SCRIPTPATH"/replacements; then
   while read -ra __; do
     go mod edit -replace ${__[0]}=${__[1]}
   done < "$SCRIPTPATH"/replacements
